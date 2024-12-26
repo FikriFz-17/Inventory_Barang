16432fa2ca4079181ae09ae0d057ba2cf3261c79
pekoy <mhmmdfaiz160@gmail.com>
2024-12-26 12:09:28 +0700

    add: sweetalert

--- user.php
+++ user.php
@@ -23,6 +23,7 @@ $users = getAllUsers($userManager);
         <link href="css/styles.css" rel="stylesheet" />
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
@@ -199,6 +200,29 @@ $users = getAllUsers($userManager);
                             </div>
                         </div>
                     </div>
+                    <?php if (!empty($_SESSION['errorMessage'])): ?>
+                        <script>
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Input Tidak Valid!',
+                                text: '<?= $_SESSION['errorMessage']; ?>',
+                                showConfirmButton: false,
+                                timer: 2000
+                            });
+                        </script>
+                        <?php unset($_SESSION['errorMessage']);?>
+                    <?php elseif (!empty($_SESSION['successMessage'])): ?>
+                        <script>
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Berhasil!',
+                                text: '<?= $_SESSION['successMessage']; ?>',
+                                showConfirmButton: false,
+                                timer: 2000
+                            });
+                        </script>
+                        <?php unset($_SESSION['successMessage']);?>
+                    <?php endif; ?>
                 </main>
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid">
